c8d304cbfe0bea3594d9c4afea90b0c2f112a0c0
dongxiaoming <dongxiaoming@jiuxiniot.com>
2020-08-03 20:50:03 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+import { ToyReact } from './ToyReact.js';
+
+
+let a = <div name="a" id="ida" >
+  <span>Helo</span>
+  <span>World</span>
+  <span>!</span>
+</div>
+
+document.body.appendChild(a);
